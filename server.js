329bc178b7b974145716823b6eb033854fd3aa9a
Bradley Olson <alllateralus@gmail.com>
2012-09-21 16:26:28 -0500

    db connection working

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ requirejs([
   'connect-flash',
   'passport',
   'passport-local',
-  'mongodb'
+  'mongodb',
   'socket.io', 
   'jade',
   'gameserver',
@@ -26,34 +26,41 @@ requirejs([
   var app = express()
     , server = http.createServer(app)
     , io = socket.listen(server)
-    , port = process.env.PORT || 8080;
+    , port = process.env.PORT || 8080
+    , db_data = {
+        server: process.env.DB_SERVER || '127.0.0.1',
+        port: process.env.DB_PORT || 27017
+      };
 
   var MongoServer = mongo.Server
-    , Db = mongo.db;
+    , MongoDb = mongo.Db;
 
-  var mongo_server = new MongoServer('localhost', 27017, {auto_reconnect: true})
-    , db = new Db('catanode_users', server);
+  var mongo_server = new MongoServer(db_data.server, db_data.port, {auto_reconnect: true});
+  var db = new MongoDb('catanode-users', mongo_server).open(function( err, client ) {
+    app.users = new mongo.Collection(client, 'users');
 
-  require('./auth')(passport_local, passport, db);
+    require('./auth')(passport_local, passport, db);
 
-  app.configure(function() {
-    app.engine('html', jade.renderFile);
-    app.set('view engine', 'jade');
-    app.set('views', __dirname + '/views');
-    app.use(express.static(__dirname + "/public/"));
-    app.use(express.cookieParser('testcookieparser'));
-    app.use(express.session({ cookie: { maxAge: 60000 }}));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.logger());
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(flash());
-    app.use(app.router);
+    app.configure(function() {
+      app.engine('html', jade.renderFile);
+      app.set('view engine', 'jade');
+      app.set('views', __dirname + '/views');
+      app.use(express.static(__dirname + "/public/"));
+      app.use(express.cookieParser('testcookieparser'));
+      app.use(express.session({ cookie: { maxAge: 60000 }}));
+      app.use(express.bodyParser());
+      app.use(express.methodOverride());
+      app.use(express.logger());
+      app.use(passport.initialize());
+      app.use(passport.session());
+      app.use(flash());
+      app.use(app.router);
+    });
+
+    require("./sockets")(app, io, gameserver, passport);
+    require("./routes")(app, io, gameserver, passport);
+
+    server.listen(port);
+    console.log( "Server running at port: " + port);
   });
-  require("./sockets")(app, io, gameserver, passport);
-  require("./routes")(app, io, gameserver, passport);
-
-  server.listen(port);
-  console.log( "Server running at port: " + port);
 });
